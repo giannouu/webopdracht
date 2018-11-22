cab26799b13c59905ee87938ffbd68c2fd3336d6
giannouu <43607112+giannouu@users.noreply.github.com>
2018-11-22 20:28:52 +0100

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<nav>
+
+<ul>
+
+<img src = "https://www.mw-webdesign.nl/images/Online-Shop.png" class = "logo">
+<input class = "search" name="search" placeholder="Search..">
+<br><br><br><br><br>
+<li><a href="index.php">Home</a></li>
+<li><a href="pagina2.php">Products</a></li>
+<li><a href="contact.php">Contact</a></li>
+<li><a href="pagina3.php">About Us</a></li>
+    
+  
+
+</ul>
+
+</nav>
+
+</html>
---
+++ pagina2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<title>Welcome</title>
+
+</head>
+
+<body>
+<header>
+
+<?php include_once("navigation.php"); ?>
+
+</header>
+</body>
+
+</html>
