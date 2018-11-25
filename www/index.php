c1237d0732701a21e574c666da03677a1d286d52
giannouu <43607112+giannouu@users.noreply.github.com>
2018-11-25 13:03:44 +0100

    Add files via upload

---
+++ www/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+<title>Welcome</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+
+
+<body>
+
+<header>
+
+<?php include_once("navigation.php"); ?>
+
+</header>
+
+<h1>The Lowest Price<br>For The Best Quality<h1>
+
+
+<a class="button" href="pagina2.php">Shop Here!</a>
+
+
+</body>
+
+</html>
